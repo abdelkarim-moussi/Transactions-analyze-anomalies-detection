fd4b808991dfb3ee69d148bf5f96c27ad26c1881
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-09-30 15:03:43 +0100

    update ClientService

--- src/main/java/com/service/ClientService.java
+++ src/main/java/com/service/ClientService.java
@@ -1,4 +1,12 @@
 package main.java.com.service;
 
+import main.java.com.entity.client.Client;
+
 public class ClientService {
+
+
+    public Client createClientAccount(String number, String email){
+
+
+    }
 }
