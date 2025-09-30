e9875d2839a0d1c942c4382be7576bbc090cf44e
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-09-30 14:42:40 +0100

    cretae client record

---
+++ src/main/java/com/business/client/Client.java
@@ -0,0 +1,10 @@
+package main.java.com.business.client;
+
+import java.util.UUID;
+
+public record Client(String id, String number, String email) {
+
+    public Client(String number , String email){
+        this(UUID.randomUUID().toString().substring(0,10),number,email);
+    }
+}
